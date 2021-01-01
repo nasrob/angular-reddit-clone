cc77582d266b689e6df3f8a1c82f6a8dee66b115
Nasser BOUKEHIL <boukehil.nasreddine@gmail.com>
2021-01-01 21:52:30 +0100

    added signup component

---
+++ src/app/auth/signup/signup.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { SignupRequestPayload } from './signup-request.payload';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit {
+
+  signupForm: FormGroup;
+  signupRequestPayload: SignupRequestPayload;
+
+  constructor() {
+    this.signupRequestPayload = {
+      username: '',
+      email: '',
+      password: '',
+    };
+  }
+
+  ngOnInit(): void {
+    this.signupForm = new FormGroup({
+      username: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', Validators.required),
+    });
+  }
+
+  signup() {
+    this.signupRequestPayload.email = this.signupForm.get('email')?.value;
+    this.signupRequestPayload.username = this.signupForm.get('username')?.value;
+    this.signupRequestPayload.password = this.signupForm.get('password')?.value;
+  }
+
+}
