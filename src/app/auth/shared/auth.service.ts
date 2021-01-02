7870185937c0a89f0d46c8f1df203327ae537740
Nasser BOUKEHIL <boukehil.nasreddine@gmail.com>
2021-01-02 14:56:39 +0100

    added login component and ngx-webstorage

--- src/app/auth/shared/auth.service.ts
+++ src/app/auth/shared/auth.service.ts
@@ -1,18 +1,36 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { LocalStorageService } from 'ngx-webstorage';
 import { Observable } from 'rxjs';
+import { LoginRequestPayload } from '../login/login.request.payload';
+import { LoginResponse } from '../login/login.response.payload';
 import { SignupRequestPayload } from '../signup/signup-request.payload';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  url: string = 'http://localhost:8080/api/auth/signup';
+  url: string = 'http://localhost:8080/api/auth';
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient,
+              private localStorage: LocalStorageService) { }
 
   signup(signupRequestPayload: SignupRequestPayload): Observable<any>{
-    return this.httpClient.post(this.url, signupRequestPayload, {responseType: 'text'})
+    return this.httpClient.post(this.url + '/signup', signupRequestPayload, {responseType: 'text'})
+  }
+
+  login(loginRequestPayload: LoginRequestPayload): Observable<boolean> {
+    return this.httpClient.post<LoginResponse>(this.url + '/login', loginRequestPayload).pipe(
+      map((data: LoginResponse) => {
+        this.localStorage.store('authenticationToken', data.authenticationToken);
+        this.localStorage.store('username', data.username);
+        this.localStorage.store('refreshToken', data.refreshToken);
+        this.localStorage.store('expiresAt', data.expiresAt);
+
+        return true;
+      })
+    );
   }
 }
