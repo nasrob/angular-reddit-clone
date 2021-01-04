ad6b1887786dde4740e0028c78991483b6929843
Nasser BOUKEHIL <boukehil.nasreddine@gmail.com>
2021-01-04 22:31:58 +0100

    added subreddit service and model

---
+++ src/app/subreddit/subreddit.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { SubredditModel } from './subreddit-response';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SubredditService {
+
+  apiUrl: string = 'http://localhost:8080/api/subreddit';
+
+  constructor(private http: HttpClient) { }
+
+  getAllSubreddits(): Observable<SubredditModel[]> {
+    return this.http.get<SubredditModel[]>(this.apiUrl);
+  }
+}
