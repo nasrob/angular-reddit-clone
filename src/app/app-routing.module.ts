1bd6db3f328a40ea3b3ef5068f5e1b1cccc580c3
Nasser BOUKEHIL <boukehil.nasreddine@gmail.com>
2021-01-06 19:02:02 +0100

    added logique to list-subreddit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,13 @@ import { SignupComponent } from './auth/signup/signup.component';
 import { HomeComponent } from './home/home.component';
 import { CreatePostComponent } from './post/create-post/create-post.component';
 import { CreateSubredditComponent } from './subreddit/create-subreddit/create-subreddit.component';
+import { ListSubredditComponent } from './subreddit/list-subreddit/list-subreddit.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'create-post', component: CreatePostComponent },
   { path: 'create-subreddit', component: CreateSubredditComponent },
+  { path: 'list-subreddits', component: ListSubredditComponent },
   { path: 'sign-up', component: SignupComponent },
   { path: 'login', component: LoginComponent }
 ];
--- src/app/subreddit/list-subreddit/list-subreddit.component.ts
+++ src/app/subreddit/list-subreddit/list-subreddit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { throwError } from 'rxjs';
+import { SubredditModel } from '../subreddit-response';
+import { SubredditService } from '../subreddit.service';
 
 @Component({
   selector: 'app-list-subreddit',
@@ -7,9 +10,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListSubredditComponent implements OnInit {
 
-  constructor() { }
+  subreddits: SubredditModel[];
+
+  constructor(private subredditService: SubredditService) { }
 
   ngOnInit(): void {
+    this.subredditService.getAllSubreddits().subscribe(data => {
+      this.subreddits = data;
+    }, error => throwError(error));
   }
-
 }
